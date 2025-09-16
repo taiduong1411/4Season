6f37a43ecf65228e80717fb6360e9fe0b0677c63
taiduong1411 <duongtai141120@gmail.com>
2025-09-16 17:58:44 +0700

    update

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -4,4 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    assetsDir: "static",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
